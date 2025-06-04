ca6858e4cf7c83f3727b90837d30fb03091fb6c4
Jahanzaib Khan <jahanzaibkhancloud@gmail.com>
2025-06-04 11:21:06 +0500

    Update start_container.sh

--- start_container.sh
+++ start_container.sh
@@ -2,7 +2,9 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-echo
+echo "Pulling image..."
+docker pull jahanzaibkhan/simple-python-flask-app
 
 # Run the Docker image as a container
-echo
+echo "Running image as container..."
+docker run -d -p 5000:5000 jahanzaibkhan/simple-python-flask-app
